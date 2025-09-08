ed04c895784552a42206ca629b659f728263ff87
requ1324 <siwieckimiki@gmail.com>
2025-09-08 18:21:54 +0200

    commit

--- portfolio/src/components/Skills/Skills.jsx
+++ portfolio/src/components/Skills/Skills.jsx
@@ -1,5 +1,4 @@
 import "./Skills.css";
-import ScrollStack, { ScrollStackItem } from "../ScrollStack/ScrollStack";
 
 export function Skills() {
   return (
@@ -7,32 +6,14 @@ export function Skills() {
       <h1 className="skills-title">
         My <span style={{ color: "#9d03fc" }}>Skills</span>
       </h1>
-      <ScrollStack>
-        <ScrollStackItem>
-          <div className="stackItem1">
-            <h2>Frontend Development</h2>
-            <p>React, JavaScript, HTML, CSS, TypeScript</p>
-          </div>
-        </ScrollStackItem>
-        <ScrollStackItem>
-          <div className="stackItem2">
-            <h2>Frontend Development</h2>
-            <p>React, JavaScript, HTML, CSS, TypeScript</p>
-          </div>
-        </ScrollStackItem>
-        <ScrollStackItem>
-          <div className="stackItem1">
-            <h2>Frontend Development</h2>
-            <p>React, JavaScript, HTML, CSS, TypeScript</p>
-          </div>
-        </ScrollStackItem>
-        <ScrollStackItem>
-          <div className="stackItem2">
-            <h2>Frontend Development</h2>
-            <p>React, JavaScript, HTML, CSS, TypeScript</p>
-          </div>
-        </ScrollStackItem>
-      </ScrollStack>
+      <div className="skillsNav">
+        <ul>
+          <li>All</li>
+          <li>Frontend</li>
+          <li>Backend</li>
+          <li>Tools</li>
+        </ul>
+      </div>
     </div>
   );
 }
